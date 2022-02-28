1cf376b45d2fe79feeb1159d1f26ef63c7aaa511
karikasg <karikas.gabor@gmail.com>
2022-02-28 10:21:48 +0100

    sentences

---
+++ src/main/java/sentences/SentenceTransformer.java
@@ -0,0 +1,24 @@
+package sentences;
+
+public class SentenceTransformer {
+
+    public String shortenSentence(String sentence) {
+        String result = sentence;
+        if (isValid(sentence)) {
+            String[] words = sentence.split(" ");
+            if (words.length >=5) {
+                result = words[0] + " ... " + words[words.length-1];
+            }
+        }
+        return result;
+    }
+
+    private boolean isValid (String sentence) {
+        Character chrFirst = sentence.charAt(0);
+        Character chrLast = sentence.charAt(sentence.length()-1);
+
+        if (chrFirst < 'A' || chrFirst > 'Z') throw new IllegalArgumentException("Must start with capital letter!");
+        if (chrLast != '.' && chrLast != '!' && chrLast != '?') throw new IllegalArgumentException("Must end with . ! or ?");
+        return true;
+    }
+}
